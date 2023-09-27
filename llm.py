1c8c007dc77b678512173b19dd553bebb48a282e
so2liu <so2liu@gmail.com>
2023-09-27 21:59:20 +0800

    init

---
+++ llm.py
@@ -0,0 +1,56 @@
+from lida import Manager, TextGenerationConfig, llm
+from lida.datamodel import Goal, ChartExecutorResponse
+import pandas as pd
+
+
+class AI():
+    def __init__(self, api_key: str) -> None:
+        self.lida = Manager(
+            text_gen=llm(
+                'openai', api_key=api_key)
+        )
+        self.gpt3_5_config = TextGenerationConfig(
+            n=1, temperature=0.5, model='gpt-3.5-turbo', use_cache=True
+        )
+        self.gpt4_config = TextGenerationConfig(
+            n=1, temperature=0.5, model='gpt-4', use_cache=True
+        )
+        self.default_config = self.gpt3_5_config
+
+    def set_config(self, type: str):
+        if type == 'gpt3_5':
+            self.default_config = self.gpt3_5_config
+        elif type == 'gpt4':
+            self.default_config = self.gpt4_config
+        else:
+            raise ValueError('type must be gpt3_5 or gpt4')
+
+    def load_df(self, df: pd.DataFrame):
+        self.df = df
+        self.summarize(df)
+
+    def summarize(self, df: pd.DataFrame):
+        self.summary = self.lida.summarize(df)
+        return self.summary
+
+    def ask_question(self, question: str,     visualization='柱状图',
+                     rationale='可以全面展示各个方面的效果') -> list[ChartExecutorResponse]:
+        lida = self.lida
+        df = self.lida.data
+        summary = self.lida.summarize(df)
+        goal = Goal(
+            question=question,
+            visualization=visualization,
+            rationale=rationale,
+            index=0
+        )
+        charts = lida.visualize(
+            summary=summary, goal=goal, library='seaborn', return_error=True)
+        return charts
+
+    def edit_chart(self,  instructions: list[str], code: str, library='seaborn') -> list[ChartExecutorResponse]:
+        lida = self.lida
+        summary = self.summary
+        charts = lida.edit(
+            summary=summary, instructions=instructions, code=code, library=library, textgen_config=self.default_config)
+        return charts
---
+++ main.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+import os
+import base64
+import chardet
+
+from llm import AI
+
+
+def show_image(base64code: str):
+    """Show image from base64 code"""
+    if ',' in base64code:
+        data = base64code.split(',')[1]
+    else:
+        data = base64code
+    binary = base64.b64decode(data)
+    st.image(binary, use_column_width=True)
+
+
+ai = None
+# input for openai-key
+openai_key = st.text_input('OpenAI Key', value='',
+                           type='password', help="回车确认")
+if openai_key:
+    hiddened_key = openai_key[:4] + '*' * \
+        (len(openai_key) - 8) + openai_key[-4:]
+    st.write(f"OpenAI Key: {hiddened_key}")
+    ai = AI(openai_key)
+
+# select model
+model_type = st.selectbox('模型类型', ['gpt3_5', 'gpt4'])
+if ai is not None:
+    ai.set_config(model_type)
+
+# upload excel or csv file
+uploaded_file = st.file_uploader("上传文件", type=["csv", "xls", "xlsx"])
+if uploaded_file is not None:
+    match uploaded_file.name.split('.')[-1]:
+        case 'xlsx' | 'xls':
+            df = pd.read_excel(uploaded_file)
+        case 'csv':
+            filepath = os.path.join('./uploads', uploaded_file.name)
+            with open(filepath, 'wb') as f:
+                f.write(uploaded_file.getbuffer())
+
+            result = chardet.detect(uploaded_file.read())
+            encoding = result['encoding']
+            df = pd.read_csv(filepath, encoding=encoding)
+        case _:
+            raise ValueError('文件格式错误')
+
+    ai.load_df(df)
+    st.write(df)
+
+# ask a question and get a chart
+question = st.text_input('问题', value='')
+chart_type = st.selectbox('图表类型', ['柱状图', '折线图', '散点图', '饼图', '箱线图', '热力图'])
+if question:
+    charts = ai.ask_question(question, visualization=chart_type)
+
+button = st.button('生成报告', type='primary')
+if button:
+    data = charts[0].raster
+    show_image(data)
+
+# edit report
+edit_advice = st.text_area('编辑建议', value='', placeholder='请在此处输入编辑建议，每行一条')
+if edit_advice:
+    instructions = edit_advice.split('\n')
+    charts = ai.edit_chart(
+        instructions=instructions, code=charts[0].code, library='seaborn')
+    show_image(charts[0].raster)
